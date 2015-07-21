4a4efd307da0c1aeb7fa5346812add929f9c6036
YU81 <yukiho0818@gmail.com>
2015-07-21 09:02:20 +0900

    Added test class of GrowthAnalytics\ClientTags .

---
+++ tests/GrowthAnalytics/ClientTagsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GrowthBeatUnOfficial\Tests\GrowthAnalytics;
+
+class ClientTagsTests extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+    }
+
+    public function testExecuteFail()
+    {
+        $clientTags    = new \GrowthBeatUnOfficial\GrowthAnalytics\ClientTags("", "", "", "");
+        $executeResult = false;
+        try {
+            $executeResult = $clientTags->execute();
+        } catch (\Exception $ex) {
+            $executeResult = false;
+        } finally {
+            $this->assertFalse($executeResult);
+        }
+    }
+}
