36543b3b8f75b9fdbf1733c488f6d799202201e4
YU81 <yukiho0818@gmail.com>
2015-07-21 09:15:56 +0900

    Renamed test class name the same as .php file name.

--- tests/GrowthAnalytics/ClientTagsTest.php
+++ tests/GrowthAnalytics/ClientTagsTest.php
@@ -2,7 +2,7 @@
 
 namespace GrowthBeatUnOfficial\Tests\GrowthAnalytics;
 
-class ClientTagsTests extends \PHPUnit_Framework_TestCase
+class ClientTagsTest extends \PHPUnit_Framework_TestCase
 {
     public function setup()
     {
